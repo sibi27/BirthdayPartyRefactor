fc039090c2bfb576d7f47e3fbd770e36020fc2e6
Rathinam Panneer <srathinampanneer@ebay.com>
2019-06-22 16:52:38 -0700

    simplified code

--- src/main/java/scrap/heap/refactor/App.java
+++ src/main/java/scrap/heap/refactor/App.java
@@ -14,24 +14,24 @@ public class App {
 
     }
 
-    private static void order(String balloonColor, String material, String number, String flavor, String frostingFlavor, String shape, String size, String cakeColor){
+    private static void order(String ... party){
 
-        orderBalloons(balloonColor, material, number);
+        orderBalloons(party[0], party[1], party[2]);
 
-        orderCake(frostingFlavor, flavor, shape, size, cakeColor);
+        orderCake(party[3], party[4], party[5], party[6], party[7]);
     }
 
-    private static void orderBalloons(String balloonColor, String material, String number){
+    private static void orderBalloons(String ... balloon){
 
         //for the purposes of this exercise, pretend this method works and adds balloons to the order
-        System.out.println("Balloons ordered; " + balloonColor + ", " + material  + ", " + number);
+        System.out.println("Balloons ordered; " + balloon[0] + ", " + balloon[1]  + ", " + balloon[2]);
 
     }
 
-    private static void orderCake(String flavor, String frostingFlavor, String shape, String size, String cakeColor){
+    private static void orderCake(String ... cake){
 
         //for the purposes of this exercise, pretend that this method adds a cake to the order
-        System.out.println("cake ordered; " + flavor + ", " + frostingFlavor  + ", " + shape + ", " + size + ", " + cakeColor);
+        System.out.println("cake ordered; " + cake[0] + ", " + cake[1]  + ", " + cake[2] + ", " + cake[3] + ", " + cake[4]);
 
     }
 
